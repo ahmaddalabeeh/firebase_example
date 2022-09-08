e2c87dc39fcc03189053be4055ff9ac2bfff3a9a
unknown <ahmad.fmd@gmail.com>
2022-09-09 01:03:10 +0300

    Clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/splash_screen.dart';
-import 'package:flutter_application_1/utilities/app_providers.dart';
-import 'package:flutter_application_1/utilities/app_routes.dart';
-import 'package:provider/provider.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,15 +13,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: appProviders,
-      child: MaterialApp(
-        title: "Firebase Example",
-        initialRoute: SplashScreen.routeName,
-        routes: appRoutes,
-      ),
+    return MaterialApp(
+      title:
+          "Firebase Example", // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
